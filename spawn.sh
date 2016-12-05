fe67b497d0e4a77e842a0209315c288337c7f026
Yousef Amar <yousefamar@gmail.com>
2016-12-05 09:54:57 +0000

    Add spawn script

---
+++ spawn.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/sh
+
+#
+# Runs a command multiple times in new terminals with different arguments
+#
+# Authors:
+# 	Yousef Amar <yousef@amar.io>
+#
+
+
+if [ $# -lt 1 ]; then
+	echo "usage: spawn.sh COMMAND [ARGS...]"
+	exit
+fi
+
+TERMINAL="evilvte"
+
+if [ $# -lt 2 ]; then
+	$TERMINAL -e "$1" &
+	exit
+fi
+
+for args in "${@:2}"
+do
+	$TERMINAL -e "$1" "$args" &
+done
